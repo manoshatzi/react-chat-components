85039508371b049a316d056ebe76ad935735f8a0
Przemysław Janowski <przemyslaw.janowski@pubnub.com>
2021-06-02 11:28:40 +0200

    feat: working monorepo setup

---
+++ data/index.js
@@ -0,0 +1,6 @@
+export { default as rawUsers } from "./json/users.json";
+export { default as rawMessages } from "./json/messages-social.json";
+export { default as loremMessages } from "./json/messages-social.json";
+export { default as socialChannels } from "./json/channels-social.json";
+export { default as directChannels } from "./json/channels-direct.json";
+export { default as workChannels } from "./json/channels-work.json";
