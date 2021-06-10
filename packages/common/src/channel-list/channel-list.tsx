2be246daa455f606841cd94ce4fd443f9f899d6a
Przemysław Janowski <przemyslaw.janowski@pubnub.com>
2021-06-10 19:30:06 +0200

    feat: poc split of ChannelList component

---
+++ packages/common/src/channel-list/channel-list.tsx
@@ -0,0 +1,61 @@
+import { ReactNode } from "react";
+import { useAtom } from "jotai";
+import { ChannelMetadataObject, ObjectCustom } from "pubnub";
+import { ThemeAtom, CurrentChannelAtom } from "../state-atoms";
+
+export interface ChannelListProps {
+  children?: ReactNode;
+  /** Pass a list of channels, including metadata, to render on the list */
+  channels: ChannelMetadataObject<ObjectCustom>[] | string[];
+  /** Provide custom channel renderer to override default themes and CSS variables. */
+  channelRenderer?: (channel: ChannelMetadataObject<ObjectCustom>) => JSX.Element;
+  /** A callback run when user clicked one of the channels. Can be used to switch current channel. */
+  onChannelSwitched?: (channel: ChannelMetadataObject<ObjectCustom>) => unknown;
+}
+
+export const useChannelListCore = (props: ChannelListProps) => {
+  const [currentChannel] = useAtom(CurrentChannelAtom);
+  const [theme] = useAtom(ThemeAtom);
+
+  /*
+  /* Helper functions
+  */
+  const isChannelActive = (ch: ChannelMetadataObject<ObjectCustom>) => {
+    return currentChannel === ch.id;
+  };
+
+  const channelSorter = (
+    a: ChannelMetadataObject<ObjectCustom>,
+    b: ChannelMetadataObject<ObjectCustom>
+  ) => {
+    return a.name.localeCompare(b.name, "en", { sensitivity: "base" });
+  };
+
+  const channelFromString = (channel: ChannelMetadataObject<ObjectCustom> | string) => {
+    if (typeof channel === "string") {
+      return {
+        id: channel,
+        name: channel,
+      };
+    }
+    return channel;
+  };
+
+  /*
+  /* Commands
+  */
+
+  const switchChannel = (channel: ChannelMetadataObject<ObjectCustom>) => {
+    if (props.onChannelSwitched) {
+      props.onChannelSwitched(channel);
+    }
+  };
+
+  return {
+    isChannelActive,
+    channelSorter,
+    channelFromString,
+    switchChannel,
+    theme,
+  };
+};
--- packages/react/src/channel-list/channel-list.tsx
+++ packages/react/src/channel-list/channel-list.tsx
@@ -1,18 +1,7 @@
-import React, { FC, ReactNode } from "react";
-import { useAtom } from "jotai";
+import React, { FC } from "react";
 import { ChannelMetadataObject, ObjectCustom } from "pubnub";
-import { ThemeAtom, CurrentChannelAtom } from "@pubnub/chat-components-common";
+import { ChannelListProps, useChannelListCore } from "@pubnub/chat-components-common";
 import "./channel-list.scss";
-
-export interface ChannelListProps {
-  children?: ReactNode;
-  /** Pass a list of channels, including metadata, to render on the list */
-  channels: ChannelMetadataObject<ObjectCustom>[] | string[];
-  /** Provide custom channel renderer to override default themes and CSS variables. */
-  channelRenderer?: (channel: ChannelMetadataObject<ObjectCustom>) => JSX.Element;
-  /** A callback run when user clicked one of the channels. Can be used to switch current channel. */
-  onChannelSwitched?: (channel: ChannelMetadataObject<ObjectCustom>) => unknown;
-}
 
 /**
  * Renders an interactive list of channels. It can represent all channels of the application, only
@@ -23,44 +12,8 @@ export interface ChannelListProps {
  * current channel passed to the Chat provider, or whatever else is expected.
  */
 export const ChannelList: FC<ChannelListProps> = (props: ChannelListProps) => {
-  const [currentChannel] = useAtom(CurrentChannelAtom);
-  const [theme] = useAtom(ThemeAtom);
-
-  /*
-  /* Helper functions
-  */
-  const isChannelActive = (ch: ChannelMetadataObject<ObjectCustom>) => {
-    return currentChannel === ch.id;
-  };
-
-  const channelSorter = (
-    a: ChannelMetadataObject<ObjectCustom>,
-    b: ChannelMetadataObject<ObjectCustom>
-  ) => {
-    return a.name.localeCompare(b.name, "en", { sensitivity: "base" });
-  };
-
-  const channelFromString = (channel: ChannelMetadataObject<ObjectCustom> | string) => {
-    if (typeof channel === "string") {
-      return {
-        id: channel,
-        name: channel,
-      };
-    }
-    return channel;
-  };
-
-  /*
-  /* Commands
-  */
-
-  const switchChannel = (channel: ChannelMetadataObject<ObjectCustom>) => {
-    if (props.onChannelSwitched) props.onChannelSwitched(channel);
-  };
-
-  /*
-  /* Renderers
-  */
+  const { isChannelActive, channelSorter, channelFromString, switchChannel, theme } =
+    useChannelListCore(props);
 
   const renderChannel = (channel: ChannelMetadataObject<ObjectCustom>) => {
     if (props.channelRenderer) return props.channelRenderer(channel);
