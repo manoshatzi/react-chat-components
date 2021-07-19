93cd46ae6275d9bf9c8a1a10f0c2958c2e648a31
Przemysław Janowski <przemyslaw.janowski@pubnub.com>
2021-07-19 17:34:31 +0200

    test: collect coverage from common code package

--- packages/react-native/jest.config.js
+++ packages/react-native/jest.config.js
@@ -1,11 +1,18 @@
 module.exports = {
   collectCoverage: true,
-  coveragePathIgnorePatterns: ["/node_modules/", "/test/", "/mock/"],
+  collectCoverageFrom: ["react-native/src/**/*.tsx", "common/src/**/*.tsx"],
+  globals: {
+    "ts-jest": {
+      tsconfig: "<rootDir>/react-native/tsconfig.json",
+    },
+  },
   preset: "react-native",
+  rootDir: "./../",
+  roots: ["<rootDir>/react-native", "<rootDir>/common"],
   setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
   transform: {
     "^.+\\.ts?$": "ts-jest",
     "^.+\\.tsx?$": "ts-jest",
-    "\\.js$": "<rootDir>/../../node_modules/react-native/jest/preprocessor.js",
+    "\\.js$": "<rootDir>/../node_modules/react-native/jest/preprocessor.js",
   },
 };
--- packages/react/jest.config.js
+++ packages/react/jest.config.js
@@ -1,12 +1,19 @@
 module.exports = {
   collectCoverage: true,
-  coveragePathIgnorePatterns: ["/node_modules/", "/test/", "/mock/"],
+  collectCoverageFrom: ["react/src/**/*.tsx", "common/src/**/*.tsx"],
+  globals: {
+    "ts-jest": {
+      tsconfig: "<rootDir>/react/tsconfig.json",
+    },
+  },
+  moduleNameMapper: {
+    "\\.(css)$": "<rootDir>/react/mock/style-mock.ts",
+    "\\.(svg)$": "<rootDir>/react/mock/svg-mock.tsx",
+  },
   preset: "ts-jest",
+  rootDir: "./../",
+  roots: ["<rootDir>/react", "<rootDir>/common"],
   setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
   testEnvironment: "jsdom",
-  moduleNameMapper: {
-    "\\.(css)$": "<rootDir>/mock/style-mock.ts",
-    "\\.(svg)$": "<rootDir>/mock/svg-mock.tsx",
-  },
   transform: { "^.+\\.(scss)$": "jest-scss-transform" },
 };
